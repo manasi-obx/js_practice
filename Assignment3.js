550056fa9081cdb15643655075a066d6f50a1b03
Manasi <manasi@openbrix.co.uk>
2022-10-11 11:24:14 +0530

    changes - import/export function

--- Assignment3.js
+++ Assignment3.js
@@ -1,7 +1,7 @@
 //---------------------1--------------------
-function greaterthat10(arr){
-    var num = arr.filter(element=>{
-        if(element>10){
+function greaterThan10 (arr) {
+    var num = arr.filter (element => {
+        if (element > 10) {
             return element;
         }
     })
@@ -9,9 +9,9 @@ function greaterthat10(arr){
 }
 
 //---------------------2--------------------
-function lessthan10(arr){
-    var num = arr.filter(element=>{
-        if(element<10){
+function lessThan10 (arr) {
+    var num = arr.filter (element => {
+        if (element < 10) {
             return element;
         }
     })
@@ -20,11 +20,11 @@ function lessthan10(arr){
 
 
 //-------------------3-------------------
-function isprime(num){
+function isPrime (num) {
         var count = 0;
-        for(var i=2; i<=Math.round(num/2); i++)
+        for (var i = 2; i <= Math.round (num / 2); i++)
         {
-            if(num % i === 0){
+            if (num % i === 0) {
               count ++;
               break;
             }
@@ -33,90 +33,84 @@ function isprime(num){
 }
 
 //-------------------4--------------------
-function primenum(arr){
-    let primearr = arr.filter(ele=>{
-        return isprime(ele);
+function primeNum (arr) {
+    let primearr = arr.filter (ele => {
+        return isPrime (ele);
     })
     return primearr;
 }
 
 //---------------------5-----------------
-function evenarray(arr){
-    let evenarr = arr.filter(element=>{
-        return element % 2 ===0;
+function evenArray (arr) {
+    let evenarr = arr.filter (element => {
+        return element % 2 === 0;
     })
     return evenarr;
 }
-function oddarray(arr){
-    let oddarr = arr.filter(element=>{
+function oddArray (arr) {
+    let oddarr = arr.filter (element => {
         return element % 2 !== 0;
     })
     return oddarr;
 }
 
 //-----------------------6------------
-function square(arr){
-    var val = arr.map(element=>{
-        return element*element;
+function square (arr) {
+    var val = arr.map (element => {
+        return element * element;
     })
     return val;
 }
 
 //-------------------7-------------
-function squareroot(arr){
-    var SR = arr.map(element=>{
+function squareRoot (arr) {
+    var SR = arr.map (element => {
         return Math.sqrt(element);
     })
     return SR;
 }
 
 //---------------8------------------
-function reversestr(str){
+function reverseString (str) {
     let revarr = [];
-    let len = str.length -1;
-    for(var i= len ; i>=0; i--)
-    {
-        revarr.push(str[i]);
+    let len = str.length - 1;
+    for (var i = len; i >= 0; i--) {
+        revarr.push (str[i]);
     }
     return revarr.join("");
 
 }
 //----------------9--------------------
-function reversenumber(num){
+function reverseNumber (num) { 
     let rem ;
     let rev = 0;
-    while(num>0)
-    {
-        rem = num%10;
-        rev = (rev*10) + rem;
-        num = parseInt(num/10);
+    while (num > 0){
+        rem = num % 10;
+        rev = (rev * 10) + rem;
+        num = parseInt(num / 10);
     }
     return rev;
 }
 //---------------10-------------------
-function palindrom(str){
-    let rev = reversestr(str);
-    if (str === rev){
+function palindrom (str) {
+    let rev = reverseString(str);
+    if (str === rev) {
         return 1;
-    }
-    else{
+    } else {
         return 0;
     }
 }
 
 //-----------------11--------------------
-function sortarray(arr){
+function sortArray (arr) {
     let newarr = [];
     var temp;
-       for(let i=0 ; i<arr.length - 1; i++)
-       {
-            for(var j=i+1 ; j<arr.length;j++)
-            {
-                if(arr[i]>arr[j])
-                {
+       for(let i = 0 ; i < arr.length - 1; i++) {
+            for(var j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
                     temp = arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 } 
             } 
         }
@@ -124,42 +118,80 @@ function sortarray(arr){
 }
 
 //-----------------12------------------
-function areaofcircle(r){
-    return 3.14*r*r;
+function areaOfCircle (r) {
+    return 3.14 * r * r;
 }
 
 //----------------------13----------------
-function areaofrect(h,w){
-    return h*w;
+function areaOfRectangle (h,w) {
+    return h * w;
 }
 //--------------------14-----------------
-function areaofsqare(w){
-    return w*w;
+function areaOfSquare (w) {
+    return w * w;
 }
 
-//----------------15---------------
-let today = new Date();
-let DOB = new Date(2000, 1, 12);
-let age = today - DOB;
-console.log(age);
+// //----------------15/1---------------
+
+// let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+// let today = new Date();
+// let tm = today.getMonth();
+// let td = today.getDate();
+// let ty = today.getFullYear();
+
+// let dob = new Date(2002, 11, 12);
+// let bm = dob.getMonth();
+// let bd = dob.getDate();
+// let by = dob.getFullYear();
+
+// let byear = ty - by;
+// let bmonth;
+//     if (tm >= bm) { 
+//         bmonth = tm - bm;
+//     } else { 
+//         byear--;
+//         bmonth = 12 + tm - bm;
+//     }
+
+
+//     if (td >= bd) {
+//      bdate = td - bd;
+//     } else {
+//         bmonth--;
+//         let days = months[tm - 2];
+//         bdate = days + td - bd;
+
+//         if (bmonth < 0) {
+//             bmonth = 11;
+//             byear--;
+//         }
+//     }
+
+// console.log(byear, bmonth, bdate);
+
+
+//-----------------15/2-------------
+
+// let today = new Date();
+// let DOB = new Date(2000, 1, 12);
+// let diff = today - DOB;
+// var totaldays = Math.floor(diff / (1000 * 60 * 60 * 24));
+// console.log(totaldays);
+
+// let years = Math.floor(totaldays / (365));
+// let rem = totaldays % (365);
+// let month = Math.floor(rem / 30.5);
+// let days = (totaldays - (years * 365) - (month * 30.5));
+
+// console.log (years,month,days);
+
 
 //------------------------------------
 
-var numbers = [36,10,45,9,2,30,44,97,7,71];
 
-console.log("Greater than 10 =",greaterthat10(numbers));
-console.log("Less than 10 =",lessthan10(numbers));
-console.log("Prime or not =",isprime(11));
-console.log("prime numbers =",primenum(numbers));
-console.log("evenarray =", evenarray(numbers));
-console.log("oddarray =", oddarray(numbers));
-console.log("square =", square(numbers));
-console.log("sortarray=",sortarray(numbers));
-console.log("reverse string =", reversestr("Manasi"));
-console.log("reverse number =", reversenumber(1234));
-console.log("Palindrom =", palindrom("123321"));
-console.log("squareroot =", squareroot([4, 16, 25]));
-console.log("Area of circle =", areaofcircle(2));
-console.log("Area of rectangle =", areaofrect(3,4));
-console.log("Area of sqare =", areaofsqare(3));
-
+module.exports = {
+    greaterThan10, lessThan10, isPrime, primeNum, evenArray, oddArray, square, sortArray, 
+    reverseString, reverseNumber, palindrom, squareRoot, areaOfCircle, areaOfRectangle,
+    areaOfSquare,
+};
